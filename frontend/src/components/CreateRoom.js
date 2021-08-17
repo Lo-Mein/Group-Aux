d9e8cb1cded8b7e0c156ea17b1db10241c9d0372
Lo <ryan.lo@fairfield.edu>
2021-08-16 23:00:01 -0400

    adding material ui

--- frontend/src/components/CreateRoom.js
+++ frontend/src/components/CreateRoom.js
@@ -1,10 +1,104 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from "react";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import { Link } from "react-router-dom";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 
 const CreateRoom = (props) =>{
+    const [pause, setPause] = useState(true);
+    const [votes, setVotes] = useState(2);
+
+    let defaultVotes = 2;
+
+    function handleVotesChange(event){
+        setVotes(event.target.value);
+    }
+
+    function handleGuestsCanPause(event){
+        event.target.value === 'true' ? setPause(true) : setPause(false);
+    }
+    
+    function handleRoomButtonPressed(){
+        const requestOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                votes_to_skip: votes,
+                guest_can_pause: pause
+            })
+        };
+        fetch('/api/create-room', requestOptions)
+        .then((response) => response.json()
+        ).then((data) => console.log(data));
+    }
 
     return (
-        <p>This is our Create Room Page</p>
+        <Grid container spacing={1}>
+            <Grid item xs={12} align="center">
+                <Typography component='h4' variant='h4'>
+                    Create A Room
+                </Typography>
+            </Grid>
+            <Grid item xs={12} align="center">
+                <FormControl component="fieldset">
+                    <FormHelperText>
+                        <div align="center">
+                            Guest Control of Playback State
+                        </div>
+                    </FormHelperText>
+                    <RadioGroup row defaultValue="true" onChange={handleGuestsCanPause}>
+                        <FormControlLabel 
+                        value="true" 
+                        control={<Radio color="primary" />}
+                        label="Play/Pause"
+                        labelPlacement="bottom" 
+                        />
+                        <FormControlLabel 
+                        value="false" 
+                        control={<Radio color="secondary" />}
+                        label="No Control"
+                        labelPlacement="bottom" 
+                        />
+                    </RadioGroup>
+                </FormControl>
+            </Grid>
+            <Grid item xs={12} align="center">
+                <FormControl>
+                    <TextField 
+                    required={true} 
+                    type="number" 
+                    onChange={ handleVotesChange }
+                    defaultValue={defaultVotes} 
+                    inputProps={{
+                        min: 1,
+                        style: {textAlign: "center"},
+                    }}
+                    />
+                    <FormHelperText>
+                        <div align="center">
+                            Votes Required To Skip Song
+                        </div>
+                    </FormHelperText>
+                </FormControl>
+            </Grid>
+            <Grid item xs={12} align="center">
+                <Button color="primary" variant="contained" onClick={handleRoomButtonPressed}>
+                    Create A Room
+                </Button>
+            </Grid>
+            <Grid item xs={12} align="center">
+                <Button color="secondary" variant="contained" to="/" component={Link}>
+                    Back
+                </Button>
+            </Grid>
+        </Grid>
     );
 }
 
